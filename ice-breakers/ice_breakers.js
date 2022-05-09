e199849f622ff5f73e83fd7efdab5d9b8e672a9c
Juho Kalermo <80675106+jpkaler@users.noreply.github.com>
2022-05-09 20:26:07 +0300

    add main object with lists

--- ice-breakers/ice_breakers.js
+++ ice-breakers/ice_breakers.js
@@ -1,10 +1,24 @@
-const questions = ['What', 'Where', 'How', 'Why'];
-const things = ['the world', 'a cat', 'Germany', 'USA', 'a dog', 'a house', 'a chair', 'the outer space', 'the sun', 'the moon', 'a life', 'the weather', 'your mum', 'a flower', 'a tree', 'the Santa Claus'];
-const verbs = [];
+const sentenceParts = {
+    _questions: ['What', 'Where', 'How', 'Why'],
+    _things: ['the world', 'a cat', 'Germany', 'USA', 'a dog', 'a house', 'a chair', 'the outer space', 'the sun', 'the moon', 'a life', 'the weather', 'your mum', 'a flower', 'a tree', 'the Santa Claus'],
+    _verbs: [],
+
+    get questions() {
+        return this._questions;
+    },
+
+    get things() {
+        return this._things;
+    },
+
+    get verbs() {
+        return this._verbs;
+    }
+}
 
 // Function to create question type eg. 'what is' or 'why does'
 const createQuestion = () => {
-    let question = questions[Math.floor(Math.random() * 4)];
+    let question = sentenceParts.questions[Math.floor(Math.random() * 4)];
     if (question === 'What' || question === 'Where') {
         return question += ' is ';
     } else {
