310702c0e54fff7d0cf76a707cd2e00d7205410c
Juho Kalermo <80675106+jpkaler@users.noreply.github.com>
2022-05-09 17:35:01 +0300

    added things and question function

--- ice-breakers/ice_breakers.js
+++ ice-breakers/ice_breakers.js
@@ -1,11 +1,18 @@
 const questions = ['What', 'Where', 'How', 'Why'];
-const things = [];
+const things = ['the world', 'a cat', 'Germany', 'USA', 'a dog', 'a house', 'a chair', 'the outer space', 'the sun', 'the moon', 'a life', 'the weather', 'your mum', 'a flower', 'a tree', 'the Santa Claus'];
 const verbs = [];
 
-// Function to create question type eg. 'what is' or 'where does'
+// Function to create question type eg. 'what is' or 'why does'
 const createQuestion = () => {
     let question = questions[Math.floor(Math.random() * 4)];
+    if (question === 'What' || question === 'Where') {
+        return question += ' is ';
+    } else {
+        return question += ' does ';
+    }
+}
 
+// Adds the topic of conversation to the question
+const addThing = (question) => {
 
-    return question;
 }
