5bb42e15c1438d9ef1ebb2157eaf7f6734ae5ab7
Juho Kalermo <80675106+jpkaler@users.noreply.github.com>
2022-05-09 21:05:36 +0300

    add random function

--- ice-breakers/ice_breakers.js
+++ ice-breakers/ice_breakers.js
@@ -1,3 +1,6 @@
+// ice_breakers.js
+// Generates a random question for you to ask when the infamous awkward silence fills the room
+
 const sentenceParts = {
     _questions: ['What', 'Where', 'How', 'Why'],
     _things: ['the world', 'a cat', 'Germany', 'USA', 'a dog', 'a house', 'a chair', 'the outer space', 'the sun', 'the moon', 'a life', 'the weather', 'your mum', 'a flower', 'a tree', 'the Santa Claus'],
@@ -16,9 +19,14 @@ const sentenceParts = {
     }
 }
 
+// Get random number
+const randomize = (num) => {
+    return Math.floor(Math.random() * num);
+}
+
 // Function to create question type e.g. 'what is' or 'why does'
 const addQuestion = () => {
-    let question = sentenceParts.questions[Math.floor(Math.random() * 4)];
+    let question = sentenceParts.questions[randomize(4)];
     if (question === 'What' || question === 'Where') {
         return question + ' is ';
     } else {
@@ -28,15 +36,14 @@ const addQuestion = () => {
 
 // Adds the topic of conversation to the question
 const addThing = () => {
-    // adds a random variable from things list to the question part
-    return addQuestion() + sentenceParts.things[Math.floor(Math.random() * sentenceParts.things.length)];
+    return addQuestion() + sentenceParts.things[randomize(sentenceParts.things.length)];
 }
 
 // Adds the verb if the question requires it (has a 'does' in it)
 const addVerb = () => {
     const sentence = addThing();
     if (sentence.includes('does')) {
-        return sentence + ' ' + sentenceParts.verbs[Math.floor(Math.random() * sentenceParts.verbs.length)] + '?';
+        return sentence + ' ' + sentenceParts.verbs[randomize(sentenceParts.verbs.length)] + '?';
     }
     return sentence + '?';
 }
